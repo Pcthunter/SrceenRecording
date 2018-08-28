fa8ceb2f746f8599cc7d16686ee6ed887efa66e6
Pcthunter <42464640+Pcthunter@users.noreply.github.com>
2018-08-28 23:10:06 +0700

    DemoSrcennRecoder

---
+++ app/src/main/java/com/example/caothang/srceenrecording/MainActivity.java
@@ -0,0 +1,255 @@
+package com.example.caothang.srceenrecording;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.SparseArray;
+import android.view.Surface;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1000;
+    private static final int REQUEST_PERMISSION = 1001;
+    private static final SparseArray ORIENTATIONS = new SparseArray();
+
+    private MediaProjectionManager mediaProjectionManager;
+    private MediaProjection mediaProjection;
+    private VirtualDisplay virtualDisplay;
+    private MediaProjectionCallback mediaProjectionCallback;
+    private MediaRecorder mediaRecorder;
+
+    private int mSrceenDensity;
+    private static final int DISPLAY_WIDTH = 720;
+    private static final int DISPLAY_HEIGHT = 1280;
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0,90);
+        ORIENTATIONS.append(Surface.ROTATION_90,0);
+        ORIENTATIONS.append(Surface.ROTATION_180,270);
+        ORIENTATIONS.append(Surface.ROTATION_270,180);
+    }
+    //view
+    private RelativeLayout rootLayout;
+    private ToggleButton toggleButton;
+    private VideoView videoView;
+    private String videoUri = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mSrceenDensity = metrics.densityDpi;
+        mediaRecorder = new MediaRecorder();
+        mediaProjectionManager = (MediaProjectionManager)getSystemService(MEDIA_PROJECTION_SERVICE);
+
+        //view
+        videoView = (VideoView) findViewById(R.id.vv_videoview);
+        toggleButton = (ToggleButton) findViewById(R.id.toggleBtn);
+        rootLayout =(RelativeLayout) findViewById(R.id.rootLayout);
+
+        //event
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)+
+                        ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED)
+                {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.RECORD_AUDIO))
+                    {
+                       toggleButton.setChecked(false);
+                        Snackbar.make(rootLayout, "Permission",Snackbar.LENGTH_INDEFINITE)
+                                .setAction("ENBLE", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        ActivityCompat.requestPermissions(MainActivity.this,
+                                                new String[]{
+                                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                        Manifest.permission.RECORD_AUDIO
+                                                },REQUEST_PERMISSION);
+                                    }
+                                }).show();
+                    }
+                    else
+                        {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                        Manifest.permission.RECORD_AUDIO
+                                },REQUEST_PERMISSION);
+                    }
+                }
+                else{
+                   toogleScreenShare(view);
+                }
+            }
+
+        });
+
+
+    }
+
+    private void toogleScreenShare(View view) {
+        if(((ToggleButton)view).isChecked()){
+            initRecorder();
+            recorderSrceen();
+        }
+    }
+
+    private void recorderSrceen() {
+        if(mediaProjection == null)
+        {
+            startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(),REQUEST_CODE);
+            return;
+        }
+        virtualDisplay = createVirtuaDisplay();
+        mediaRecorder.start();
+    }
+
+    private VirtualDisplay createVirtuaDisplay() {
+        return mediaProjection.createVirtualDisplay("MainActivity",DISPLAY_HEIGHT,DISPLAY_WIDTH,mSrceenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mediaRecorder.getSurface(),null,null);
+    }
+
+    private void initRecorder() {
+        try{
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+            videoUri = Environment.getExternalStorageState(new File(Environment.DIRECTORY_DOWNLOADS))
+                    + new StringBuilder("/").append(new SimpleDateFormat("EDMT_Record_dd-MM-yyyy-hh_mm_ss").format(new Date())).append(".mp4").toString();
+
+            mediaRecorder.setOutputFile(videoUri);
+            mediaRecorder.setVideoSize(DISPLAY_WIDTH,DISPLAY_HEIGHT);
+            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setVideoEncodingBitRate(512*1000);
+            mediaRecorder.setVideoFrameRate(30);
+
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            int orientation = (int) ORIENTATIONS.get(rotation*90);
+            mediaRecorder.setOrientationHint(orientation);
+            mediaRecorder.prepare();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode != REQUEST_CODE)
+        {
+            Toast.makeText(this,"Unk Error",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (resultCode != RESULT_OK)
+        {
+            Toast.makeText(this,"Permission dendied",Toast.LENGTH_SHORT).show();
+            toggleButton.setChecked(false);
+            return;
+        }
+        mediaProjectionCallback = new MediaProjectionCallback();
+        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode,data);
+        mediaProjection.registerCallback(mediaProjectionCallback,null);
+        virtualDisplay = createVirtuaDisplay();
+        mediaRecorder.start();
+    }
+
+    private class MediaProjectionCallback extends MediaProjection.Callback {
+        @Override
+        public void onStop() {
+            if (toggleButton.isChecked())
+            {
+                toggleButton.setChecked(false);
+                mediaRecorder.stop();
+                mediaRecorder.reset();
+            }
+            mediaProjection = null;
+            stopRecordScreen();
+            super.onStop();
+        }
+    }
+
+    private void stopRecordScreen() {
+        if(virtualDisplay == null){
+            return;
+        }
+        virtualDisplay.release();
+        destroyMediaProjection();
+    }
+
+    private void destroyMediaProjection() {
+        if(mediaProjection != null)
+        {
+            mediaProjection.unregisterCallback(mediaProjectionCallback);
+            mediaProjection.stop();
+            mediaProjection = null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case REQUEST_PERMISSION:
+            {
+                if((grantResults.length > 0) && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED))
+                {
+                    toogleScreenShare(toggleButton);
+                }
+                else
+                {
+                    toggleButton.setChecked(false);
+                    Snackbar.make(rootLayout, "Permission",Snackbar.LENGTH_INDEFINITE)
+                            .setAction("ENBLE", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    ActivityCompat.requestPermissions(MainActivity.this,
+                                            new String[]{
+                                                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                    Manifest.permission.RECORD_AUDIO
+                                            },REQUEST_PERMISSION);
+                                }
+                            }).show();
+                }
+                return;
+            }
+        }
+    }
+}
